8624d52d9d65e6e84e7a76e5dc0c7a0d2c256d89
root <root@ip-172-31-16-140.us-east-2.compute.internal>
2019-09-26 10:12:59 +0000

    file

--- main.tf
+++ main.tf
@@ -12,11 +12,13 @@ resource "aws_vpc" "vpc_ex" {
 }
 
 resource "aws_subnet" "subnets" {
-  count      = "${length(data.aws_availability_zones.azs.names)}"
-  vpc_id     = "${aws_vpc.vpc_ex.id}"
-  cidr_block = "${element(var.subnet_cidr,count.index)}"
+  count             = "${length(data.aws_availability_zones.azs.names)}"
+  availability_zone = "${element(data.aws_availability_zones.azs.names,count.index)}"
+  vpc_id            = "${aws_vpc.vpc_ex.id}"
+  cidr_block        = "${element(var.subnet_cidr,count.index)}"
 
   tags = {
     Name = "subnet-${count.index+1}"
   }
+
 }
